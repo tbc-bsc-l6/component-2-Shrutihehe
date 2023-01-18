ee7ebd727f961fd168f25836d79bf3b2d0537b16
Shrutihehe <rshruti21@tbc.edu.np>
2023-01-18 20:45:41 +0545

    worked on submit data, database, routes

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Psy\Command\WhereamiCommand;
 
 class TestsController extends Controller
 {
@@ -13,70 +14,73 @@ class TestsController extends Controller
 
         //has user registered for this exam
         $student_already_registered = DB::table('students')->where('user_id', Auth::user()->id)->where('subject_id', $subject_id)->exists();
-        $has_taken_exam = DB::table('results')->where('user_id', Auth::user()->id)->where('subject_id',$subject_id)->exists();
+        $has_taken_exam = DB::table('results')->where('user_id', Auth::user()->id)->where('subject_id', $subject_id)->exists();
 
         if ($has_taken_exam) {
             return \redirect()->route('main')->with('hasTakenExam', 'You have alrwady given the test');
-
-        }
-        
-        elseif (!$student_already_registered) {//if the user has not registered
+        } elseif (!$student_already_registered) { //if the user has not registered
             return \redirect()->route('dashboard')->with('registerFirst', 'Please regsiter for exam');
-            
-           
+
+
 
             //user hasalready regsitered 
         } else {
             //check if user has already taken the exam
-            
-            
+
+
             if ($has_taken_exam) {
                 return \redirect()->route('main')->with('hasTakenExam', 'You have already given the test');
             } else {
-              $questions = DB::table('tests')->where('subject_id',$subject_id)->get();
-              $exam_deadline= DB::table('subjects')->where('id',$subject_id)->value('exam_deadline');
-              $duration = DB::table('subjects')->where('id',$subject_id)->value('duration');
-              return view('test', [
-                'questions' => $questions,
-                'subject_id' =>$subject_id,
-                'exam_deadline' =>$exam_deadline,
-                'duration'=>$duration
-            ]);
-              
+                $questions = DB::table('tests')->where('subject_id', $subject_id)->get();
+                $exam_deadline = DB::table('subjects')->where('id', $subject_id)->value('exam_deadline');
+
+                //if students starts the exam for the first time then return this
+                $duration = DB::table('subjects')->where('id', $subject_id)->value('duration');
+           
+                
+
+                   
+                        return view('test', [
+                            'questions' => $questions,
+                            'subject_id' => $subject_id,
+                            'exam_deadline' => $exam_deadline,
+                            'duration' => $duration
+                        ]);
+                    }
+                
             }
         }
-    }
-
-
-    public function allResults(){
-            //get all the results that belongs to the currently logged in user
-            $allResults = DB::table('results')->where('user_id', Auth::user()->id)->get();
-            return view('table.allResults',['allResults'=>$allResults]);
-           //return ("hello");
-
-
-
-
-    }
-
-    public function allTests(){
-        $subjects = DB::table('students')->where('user_id',Auth::user()->id)
-        ->join('subjects','students.subject_id', '=','subjects.id')->get();
-
-        return view('table.allTests',['subjects'=>$subjects]);
         
 
+    public function allResults()
+    {
+        //get all the results that belongs to the currently logged in user
+        $allResults = DB::table('results')->where('user_id', Auth::user()->id)->get();
+        return view('table.allResults', ['allResults' => $allResults]);
+        //return ("hello");
+
+
+
+
+    }
+
+    public function allTests()
+    {
+        $subjects = DB::table('students')->where('user_id', Auth::user()->id)
+            ->join('subjects', 'students.subject_id', '=', 'subjects.id')->get();
+            
+
+        return view('table.allTests', ['subjects' => $subjects]);
     }
 
     public function registerExam(Request $request, $subject_id)
     {
         //check if user already registered
         $student_already_register = DB::table('students')->where('user_id', Auth::user()->id)->where('subject_id', $subject_id)->exists();
-        $has_taken_exam = DB::table('results')->where('user_id', Auth::user()->id)->where('subject_id',$subject_id)->exists();
-        if ($has_taken_exam){
+        $has_taken_exam = DB::table('results')->where('user_id', Auth::user()->id)->where('subject_id', $subject_id)->exists();
+        if ($has_taken_exam) {
             return \redirect()->route('dashbaord')->with('hasTakenExam', 'You have already given the test');
-
-        }elseif ($student_already_register) {
+        } elseif ($student_already_register) {
 
             return \redirect()->route('dashboard')->with('alreadyRegisteredForExam', 'You have already regsitered for exam');
         } else {
@@ -101,9 +105,9 @@ class TestsController extends Controller
         //to calculate points 
         $points = 0;
         $percentage = 0;
-        $totalQuestion = 2;
+        $totalQuestion = 8;
 
-        $subjectId=$request->input('subjectId');
+        $subjectId = $request->input('subjectId');
 
 
 
@@ -126,20 +130,23 @@ class TestsController extends Controller
         $percentage = ($points / $totalQuestion) * 100;
         //dd($percentage);
 
-        $subjectName= DB::table('subjects')->where('id',$subjectId)->value('name');
+        $subjectName = DB::table('subjects')->where('id', $subjectId)->value('name');
         $id = Auth::user()->id;
         //insert score in the results table 
         DB::table('results')->insert([
             'user_id' => $id,
             'score' => $percentage,
             'subject_id' => $subjectId,
-            'subject_name'=>$subjectName
+            'subject_name' => $subjectName
         ]);
 
 
         //remove student info from student table 
-        DB::table('students')->where('user_id',Auth::user()->id)->where('subject_id',$subjectId)->delete();
+        DB::table('students')->where('user_id', Auth::user()->id)->where('subject_id', $subjectId)->delete();
         // return to main page
         return redirect()->route('main')->with('examSubmitted', 'Your exam has been submitted successfully, Please click rsult to know the score.');
     }
+
+
+  
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,15 +6,19 @@
 
 <div class="container mt-3">
 
-<div id="deadline" class="alert alert-warning"> </div>
+  <div id="deadline" class="alert alert-warning"> </div>
 
 
   <form action="{{route('submitExam')}}" method="Post">
     {{csrf_field()}}
     <input type="hidden" class="form-control" name="subjectId" value="{{$subject_id}}">
-    @foreach($questions as $question)
+    
 
-    <fieldset class="mt-3" id="{{$question->id}}">
+    @foreach($questions as $question)
+    <fieldset class="mt-3" id="">
+    <div class="row">
+      <div class="col">
+        
       <h5>{{ $question->question}}</h5>
       <div class="form-check">
         <input class="form-check-input" type="radio" name="{{$question->id}}" value="1" id="flexRadioDefault1">
@@ -40,9 +44,17 @@
           {{$question->answer4}}
         </label>
       </div>
+
+      </div>
+   
 </div>
-</fieldset>
+
+
+    </div>
+
+    </fieldset>  
 @endforeach
+
 
 <button id="submitExam" class="btn btn-primary mt=3" type="submit"> finish exam </button>
 
@@ -50,24 +62,30 @@
 </div>
 
 <script>
+  var duration = '{{$duration}}'* 60;
+  var time = duration;
+  var deadline = document.getElementById('deadline');
+ 
+  setInterval(function() {
+        var counter = time--,
+          min = (counter / 60) >> 0,
+          sec = (counter - min * 60) + '';
+        deadline.textContent = 'Exam closes in ' + min + ':' + (sec.length > 1 ? '' : '0') + sec
+        //time!=0|| (time=duration);
+        //timer.innerHTML = min;
 
-var duration = {{$duration}} * 60;
-var time=duration;
-var deadline = document.getElementById('deadline');
-setInterval(function(){
-var counter=time--,min=(counter/60)>>0, sec=(counter-min*60)+'';
-deadline.textContent='Exam closes in '+min+':'+(sec.length>1?'': '0')+sec
-//time!=0|| (time=duration);
-//timer.innerHTML = min;
+
+        if (counter == 0) {
+          //submmit the exam automatically
+          document.getElementById('submitExam').click();
+
+        }
+
+        
 
 
-if(counter==0){
-  //submmit the exam automatically
-  document.getElementById('submitExam').click();
-
-}
-},1000);
-  </script>
+        }, 1000);
+</script>
 
 
 @endsection
