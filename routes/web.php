0cc31cf954de910425d5de666ba51794700c2bb1
Shrutihehe <rshruti21@tbc.edu.np>
2023-01-20 00:18:46 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,13 @@ Route::get('/', function () {
     return view('index');
 })->name('main');
 
-Route::get('/test/{subject_id}', [TestsController::class, "getTestQuestions"])->name('getTestQuestion')->middleware('auth'); //only allows to take test when login. 
+Route::get('/test/{subject_id}', [TestsController::class, "getTestQuestions"])->name('getTestQuestion')->middleware('auth'); //only allows to take test when login.
 
 Route::post('/submitExam', [TestsController::class, "submitExam"])->name('submitExam');
 
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        //get data from database
-        $data = "this is data";
-
-        //get all subjects
-        $subjects = DB::table('subjects')->get();
-
-        return view('dashboard',['data'=>$data,'subjects'=>$subjects]);
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::get('dashboard',[\App\Http\Controllers\DashboardController::class, "dashboard"])->name('dashboard');
 });
 Route::get('/register_exam/{subject_id}',[TestsController::class,"registerExam"])->name('registerExam');
 
